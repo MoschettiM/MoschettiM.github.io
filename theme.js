638f285b896c1baf874fc8c05d176477b7d7a9bf
MoschettiM <m.moschetti@unsw.edu.au>
2023-06-08 21:07:00 +1000

    Trying to get this thing working...

--- theme.js
+++ theme.js
@@ -1,10 +1,16 @@
-document.getElementById('mode-switch').addEventListener('click', function () {
-    let theme = document.getElementById('theme-style').href;
-    if (theme.includes('light')) {
-        document.getElementById('theme-style').href = 'dark.css';
+const modeSwitch = document.getElementById('mode-switch');
+const themeLink = document.getElementById('theme-style');
+const body = document.body;
+modeSwitch.addEventListener('click', function () {
+    if (body.classList.contains('light-theme')) {
+        body.classList.remove('light-theme');
+        body.classList.add('dark-theme');
+        themeLink.href = 'dark.css';
         this.textContent = 'Light';
     } else {
-        document.getElementById('theme-style').href = 'light.css';
+        body.classList.remove('dark-theme');
+        body.classList.add('light-theme');
+        themeLink.href = 'light.css';
         this.textContent = 'Dark';
     }
 });
